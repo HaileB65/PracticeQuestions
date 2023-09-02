d5e8e6e470eefc631743d3457742078f8f3e2023
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-01 20:03:34 -0500

    Mentor Review: Updating NewYearsChaos method

---
+++ src/MarkAndToys.java
@@ -0,0 +1,43 @@
+public class MarkAndToys {
+    public static void main(String[] args) {
+        findMaximumNumberOfItems();
+    }
+
+    private static void findMaximumNumberOfItems() {
+        int[] prices = {1, 2, 3, 4};
+        int k = 7;
+        int numberOfItems = 0;
+        int total = 0;
+
+        for (int i = 0; i <= prices.length - 1; i++) {
+            total = total + prices[i];
+
+            if (total >= 7) {
+                numberOfItems = numberOfItems + 1;
+                break;
+            }
+            numberOfItems = numberOfItems + 1;
+        }
+
+        System.out.println(total);
+        System.out.println(numberOfItems);
+
+        int total2 = 0;
+        int numberOfItems2 = 0;
+        for (int i = 0; i <= prices.length - 1; i++) {
+            total2 = total2 + prices[prices.length - 1 - i];
+
+            if (total2 == 7) {
+                numberOfItems2 = numberOfItems2 + 1;
+                continue;
+            }
+            if (total2 < 7) {
+                numberOfItems2 = numberOfItems2 + 1;
+            }
+        }
+
+        System.out.println();
+        System.out.println(total2);
+        System.out.println(numberOfItems2);
+    }
+}
--- src/NewYearsChaos.java
+++ src/NewYearsChaos.java
@@ -4,23 +4,35 @@ import java.util.List;
 public class NewYearsChaos {
 
     public static void main(String[] args) {
-        int[] array = {2,1,5,3,4};
+        List<Integer> array = List.of(2,1,5,3,4);
+        List<Integer> array2 = List.of(1, 2, 5, 3, 7, 8, 6, 4);
 
-        findMinimumNumberOfBribes(array);
+        findMinimumNumberOfBribes(array2);
+
     }
 
-    public static void findMinimumNumberOfBribes(int[] finalStateOfQueue){
+    public static void findMinimumNumberOfBribes(List<Integer> q){
         int totalBribes = 0;
-        List<Integer> peopleWhoBribed = new ArrayList<>();
 
-        for (int i=0; i <= finalStateOfQueue.length-1; i++){
-            if(finalStateOfQueue[finalStateOfQueue.length-1-i] < finalStateOfQueue.length-i){
-                totalBribes += 1;
-                peopleWhoBribed.add(finalStateOfQueue.length - i);
+        for (int i=0; i <= q.size()-1; i++){
+            int actualNumber = q.get(q.size() - 1 - i);
+            int expectedNumber = q.size()-i;
+
+//            if(q.get(i) > q.get(i)){
+//                totalBribes += totalBribes + 1;
+//            }
+
+            if(actualNumber > expectedNumber){
+                int bribes = actualNumber-expectedNumber;
+                totalBribes += (actualNumber - expectedNumber);
+
+                if(bribes > 2){
+                    System.out.println("Too chaotic");
+                    return;
+                }
             }
         }
 
         System.out.println(totalBribes);
-        System.out.println(peopleWhoBribed);
     }
 }
