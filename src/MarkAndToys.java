b7c1f866666863c2a51070121b7b0269acb5e2e2
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-07 13:41:05 -0500

    Changed integer array to list.

--- src/MarkAndToys.java
+++ src/MarkAndToys.java
@@ -1,16 +1,18 @@
+import java.util.List;
+
 public class MarkAndToys {
     public static void main(String[] args) {
         findMaximumNumberOfItems();
     }
 
     private static void findMaximumNumberOfItems() {
-        int[] prices = {1, 2, 3, 4};
+        List<Integer> prices = List.of(1, 2, 3, 4);
         int k = 7;
         int numberOfItems = 0;
         int total = 0;
 
-        for (int i = 0; i <= prices.length - 1; i++) {
-            total = total + prices[i];
+        for (int i = 0; i <= prices.size() - 1; i++) {
+            total = total + prices.get(i);
 
             if (total >= 7) {
                 numberOfItems = numberOfItems + 1;
@@ -24,8 +26,8 @@ public class MarkAndToys {
 
         int total2 = 0;
         int numberOfItems2 = 0;
-        for (int i = 0; i <= prices.length - 1; i++) {
-            total2 = total2 + prices[prices.length - 1 - i];
+        for (int i = 0; i <= prices.size() - 1; i++) {
+            total2 = total2 + prices.get(prices.size() - 1 - i);
 
             if (total2 == 7) {
                 numberOfItems2 = numberOfItems2 + 1;
