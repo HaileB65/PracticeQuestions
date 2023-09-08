659df7100998f1f294f2ece0427ace13e0517f26
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-07 20:24:20 -0500

    Mentor review/update: Fixed issues with MarkAndToys problem.

--- src/MarkAndToys.java
+++ src/MarkAndToys.java
@@ -1,18 +1,21 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class MarkAndToys {
     public static void main(String[] args) {
-        findMaximumNumberOfItems();
-    }
-
-    private static void findMaximumNumberOfItems() {
         //example 1
 //        List<Integer> prices = List.of(1, 2, 3, 4);
 //        int k = 7;
 
         //example 2
-        List<Integer> prices = List.of(1, 12, 5, 111, 200, 1000, 10);
+        List<Integer> prices = new ArrayList<>(List.of(1, 12, 5, 111, 200, 1000, 10));
         int k = 50;
+
+        findMaximumNumberOfItems(prices, k);
+    }
+
+    private static int findMaximumNumberOfItems(List<Integer> prices, int k) {
+        prices.sort(null);
 
         int numberOfItems = 0;
         int total = 0;
@@ -30,22 +33,6 @@ public class MarkAndToys {
             numberOfItems = numberOfItems + 1;
         }
 
-        int total2=0;
-        int numberOfItems2 = 0;
-
-        for (int i = prices.size() - 1; i >= 0 ; i--) {
-            if(total2 == k){
-                break;
-            }
-
-            if((total2 + prices.get(i)) > k){
-                break;
-            }
-
-            total2 = total2 + prices.get(i);
-            numberOfItems2 = numberOfItems2 + 1;
-        }
-
-        System.out.println(Math.max(numberOfItems, numberOfItems2));
+        return numberOfItems;
     }
 }
---
+++ src/MinimumSwaps2.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumSwaps2 {
+
+    public static void main(String[] args) {
+        minimumSwaps();
+    }
+
+    private static void minimumSwaps() {
+        int[] arr = {7, 1, 3, 2, 4, 5, 6};
+
+        int lowestNumber = 0;
+        int highestNumber = 0;
+
+        //find the lowest number and switch it with the number in 0 index
+        //find the highest number and switch it with the number in the last index
+
+    }
+}
