247ec824528385b9514ff49fa0ffa3cbb9bace8d
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-14 17:03:14 -0500

    Updating MinimumSwaps2 class.

--- src/MinimumSwaps2.java
+++ src/MinimumSwaps2.java
@@ -1,20 +1,43 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MinimumSwaps2 {
 
     public static void main(String[] args) {
-        minimumSwaps();
-    }
-
-    private static void minimumSwaps() {
         int[] arr = {7, 1, 3, 2, 4, 5, 6};
 
-        int lowestNumber = 0;
-        int highestNumber = 0;
+        minimumSwaps(arr);
 
-        //find the lowest number and switch it with the number in 0 index
-        //find the highest number and switch it with the number in the last index
+    }
 
+    private static void minimumSwaps(int[] arr) {
+        System.out.println("Original Array = " + Arrays.toString(arr));
+
+        int[] sortedArray = new int[arr.length];
+        for(int i=0 ; i < arr.length; i++){
+            sortedArray[i] = arr[i];
+        }
+        Arrays.sort(sortedArray);
+
+        System.out.println("Sorted Array = " + Arrays.toString(sortedArray));
+
+        int count = 0;
+        int tempNumber =0;
+        for(int i=0; i < arr.length; i++) {
+            if (arr[i] > sortedArray[i]) {
+                arr[i] = sortedArray[i];
+                count++;
+                //switch 7 with 2
+                tempNumber = arr[i];
+//                Arrays.indexOf(arr, 3);
+            }
+            if (arr[i] < sortedArray[i]) {
+                arr[i] = sortedArray[i];
+                count++;
+            }
+        }
+
+        System.out.println("Final array = " + Arrays.toString(arr));
+        System.out.println("Count to sort array = " + count);
     }
 }
