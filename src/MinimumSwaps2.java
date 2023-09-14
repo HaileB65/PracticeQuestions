02ed29098f2a6a808423e16c3c3d74367450ebac
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-14 17:55:03 -0500

    Updated to minimumswaps method.

--- src/MinimumSwaps2.java
+++ src/MinimumSwaps2.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MinimumSwaps2 {
 
@@ -21,15 +22,12 @@ public class MinimumSwaps2 {
 
         System.out.println("Sorted Array = " + Arrays.toString(sortedArray));
 
+        //draft 1 not swapping elements
         int count = 0;
-        int tempNumber =0;
         for(int i=0; i < arr.length; i++) {
             if (arr[i] > sortedArray[i]) {
                 arr[i] = sortedArray[i];
                 count++;
-                //switch 7 with 2
-                tempNumber = arr[i];
-//                Arrays.indexOf(arr, 3);
             }
             if (arr[i] < sortedArray[i]) {
                 arr[i] = sortedArray[i];
@@ -39,5 +37,23 @@ public class MinimumSwaps2 {
 
         System.out.println("Final array = " + Arrays.toString(arr));
         System.out.println("Count to sort array = " + count);
+
+        //draft 2 swapping elements
+//        int count1 = 0;
+//        int tempNumber =0;
+//        for(int i=0; i < arr.length; i++) {
+//            if (arr[i] > sortedArray[i]) {
+//                tempNumber = arr[i];
+//                int index = List.of(arr).indexOf(1);
+//                System.out.println(index);
+//                arr[index] = tempNumber;
+//                arr[i] = sortedArray[i];
+//                count1++;
+//            }
+//            if (arr[i] < sortedArray[i]) {
+//                arr[i] = sortedArray[i];
+//                count++;
+//            }
+//        }
     }
 }
