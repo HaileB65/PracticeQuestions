14d8b6e90720bd0a7d4fa70edbe9778d054a2b0e
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-14 20:12:03 -0500

    Mentor edit: Made MinimumSwaps2 method work. Still need to make method more efficient.

--- src/MinimumSwaps2.java
+++ src/MinimumSwaps2.java
@@ -6,12 +6,28 @@ public class MinimumSwaps2 {
 
     public static void main(String[] args) {
         int[] arr = {7, 1, 3, 2, 4, 5, 6};
+        int[] arr2 = {4, 3, 1, 2};
+        int[] arr3 = {2, 3, 4, 1, 5};
+        int[] arr4 = {1, 3, 5, 2, 4, 6, 7};
 
-        minimumSwaps(arr);
+//        int minSwaps = minimumSwaps(arr);
 
+//        int minSwaps2 = bubbleBubbleCount(arr);
+
+        int minSwaps3 = bubbleBubbleCount2(arr);
+        System.out.println(minSwaps3);
+
+        minSwaps3 = bubbleBubbleCount2(arr2);
+        System.out.println(minSwaps3);
+
+        minSwaps3 = bubbleBubbleCount2(arr3);
+        System.out.println(minSwaps3);
+
+        minSwaps3 = bubbleBubbleCount2(arr4);
+        System.out.println(minSwaps3);
     }
 
-    private static void minimumSwaps(int[] arr) {
+    private static int minimumSwaps(int[] arr) {
         System.out.println("Original Array = " + Arrays.toString(arr));
 
         int[] sortedArray = new int[arr.length];
@@ -39,21 +55,67 @@ public class MinimumSwaps2 {
         System.out.println("Count to sort array = " + count);
 
         //draft 2 swapping elements
-//        int count1 = 0;
-//        int tempNumber =0;
-//        for(int i=0; i < arr.length; i++) {
-//            if (arr[i] > sortedArray[i]) {
-//                tempNumber = arr[i];
-//                int index = List.of(arr).indexOf(1);
-//                System.out.println(index);
-//                arr[index] = tempNumber;
-//                arr[i] = sortedArray[i];
-//                count1++;
-//            }
-//            if (arr[i] < sortedArray[i]) {
-//                arr[i] = sortedArray[i];
-//                count++;
-//            }
-//        }
+        int count1 = 0;
+        int tempNumber =0;
+        for(int i=0; i < arr.length; i++) {
+            if (arr[i] > sortedArray[i]) {
+                tempNumber = arr[i];
+                int index = List.of(arr).indexOf(1);
+                System.out.println(index);
+                arr[index] = tempNumber;
+                arr[i] = sortedArray[i];
+                count1++;
+            }
+            if (arr[i] < sortedArray[i]) {
+                arr[i] = sortedArray[i];
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // {7, 1, 3, 2, 4, 5, 6}
+    private static int bubbleBubbleCount(int[] arr){
+        int tempNumber = 0;
+        int swapCount = 0;
+
+        for(int j = 0; j < arr.length-1; j++) {
+            for (int i = 0; i < arr.length - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    tempNumber = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tempNumber;
+                    swapCount++;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    private static int bubbleBubbleCount2(int[] arr){
+        int tempNumber = 0;
+        int swapCount = 0;
+
+
+        if(arr[0] > arr[arr.length/2]){
+            tempNumber = arr[0];
+            arr[0] = arr[arr.length/2];
+            arr[arr.length/2] = tempNumber;
+            swapCount++;
+        }
+
+        for(int j = 0; j < arr.length-1; j++) {
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    tempNumber = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tempNumber;
+                    swapCount++;
+                }
+            }
+        }
+
+        return swapCount;
     }
 }
