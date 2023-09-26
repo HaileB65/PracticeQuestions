f7bc12c6948cf8f162b0c75ff6b1a8297b0e0786
HaileB65 <92234955+HaileB65@users.noreply.github.com>
2023-09-26 17:09:58 -0500

    Got numberOfAnagrammaticPairs method to work on two example strings.

--- src/SherlockAndAnagrams.java
+++ src/SherlockAndAnagrams.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class SherlockAndAnagrams {
 
     public static void main(String[] args) {
-        String s = "mom";
-        List<String> strings = List.of("abba", "abcd");
+        String a = "mom";
+        String b ="abba";
+        String c = "abcd";
 
-        System.out.println(numberOfAnagrammaticPairs(s));
+        System.out.println(numberOfAnagrammaticPairs(a));
+        System.out.println(numberOfAnagrammaticPairs(b));
+
 
     }
 
@@ -16,30 +19,28 @@ public class SherlockAndAnagrams {
 
         for (int i = s.length() - 1; i >= 0; i--) { // for loop for length of string
             if (charCount.containsKey(s.charAt(i))) { // if hashmap contains char in string at index 'i'
-                int count = charCount.get(s.charAt(i)); // get value(count) of key(char) from hashmap
-                charCount.put(s.charAt(i), ++count); // adds key with value of (count) to the hashmap
+                int count = charCount.get(s.charAt(i)); // get the count of letter in string
+                charCount.put(s.charAt(i), ++count); // adds char(key) with count(value) to the hashmap
             } else {
-                charCount.put(s.charAt(i), 1); // adds key with value of 1 to hashmap
+                charCount.put(s.charAt(i), 1); // adds char with count of 1 to hashmap
             }
         }
 
-        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
-            Character key = entry.getKey();
-            Integer value = entry.getValue();
+//        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) { // iterates over hashmap
+//            Character key = entry.getKey();
+//            Integer value = entry.getValue();
+//
+//            if(value%2 == 0) { // checks to see if char in hashmap has a value divisible by 2.
+//                anagramPairCount =  anagramPairCount + (value/2); // if so the anagramPairCount is increased by value divided by 2.
+//            }
+//        }
 
-            if(value%2 == 0) { // checks to see if char in hashmap has a value divisible by 2.
-                anagramPairCount =  anagramPairCount + (value/2); // if so the anagramPairCount is increased by value divided by 2.
+        for(int i = 1; i < s.length(); i++) {
+            String subString = s.substring(i);
+            String flippedSubString = reverseSubString(subString);
+            if (s.contains(flippedSubString)) {
+                anagramPairCount++;
             }
-        }
-
-
-        // how can "mo" be rearranged in string "mom" without using the first "m". Yes, as "om".
-        System.out.println(s.substring(1)); // prints out "om"
-        String subString = s.substring(1);
-        // does substring "mo" flipped exist in "mom"?
-        String flippedSubString = reverseSubString(subString);
-        if (s.contains(flippedSubString)) {
-         anagramPairCount++;
         }
 
         return anagramPairCount;
